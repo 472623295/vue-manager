26a53321a60e9a76d3bf5585998258716bae8526
472623295 <472623295@qq.com>
2020-05-11 23:25:20 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+// 导入样式
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
 new Vue({
